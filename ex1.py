48c84ba12328db3c2c1522faf0df1c6c4fc86c7c
jothibasuj <jothibasuj@dckap.com>
2018-07-26 15:26:12 +0530

    added git command

--- ex1.py
+++ ex1.py
@@ -1,1 +1,5 @@
 print "welcome to git comment learning"
+
+# This will destroy any local modifications.
+# Don't do it if you have uncommitted work you want to keep.
+git reset --hard 4f87f48897e13029c7826babf761ad44fe65abfc

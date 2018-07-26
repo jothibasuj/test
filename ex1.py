0afc1bfe1f7128f05ddbfc150749c5c0d4200e58
jothibasuj <jothibasuj@dckap.com>
2018-07-26 15:44:17 +0530

    made changes

--- ex1.py
+++ ex1.py
@@ -8,6 +8,8 @@ git reset --hard 4f87f48897e13029c7826babf761ad44fe65abfc
 
 # Resets index to former commit; replace '56e05fced' with your commit code
 git reset 56e05fced 
+
+
 # Moves pointer back to previous HEAD
 git reset --soft HEAD@{1}
 git commit -m "Revert to 56e05fced"
